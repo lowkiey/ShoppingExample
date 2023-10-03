97b8e3301f6bee0c8f6c3ec4b116c6b214b9ed5d
DoniaAli77 <donia.ali.7799@gmail.com>
2023-10-03 21:21:32 +0300

    product

---
+++ Models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const productSchema = require('./productModel').Schema;
+const schemaOptions = {
+  strict: false,
+  timestamps: true,
+};
+const userschema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minLength: 3,
+      maxLength: 30,
+    },
+    userType: {
+      type: String,
+      required: true,
+    },
+    shoppingCart:[productSchema],
+  },
+  // schemaOptions
+  {
+    strict: false,
+    timestamps: true,
+  }
+);
+
+
+module.exports = mongoose.model('userModel', userschema);
